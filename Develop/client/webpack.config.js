55eb0838ac7edcf36f4416d17c01bb00d7897ff7
wyamet <wyattdomanski.md@gmail.com>
2023-03-17 12:34:51 -0700

    added workbox plugins, css loader and babel

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,29 +1,65 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        chunks: ["main"],
+        filename: "index.html",
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/install.html",
+        chunks: ["install"],
+        filename: "install.html",
+      }),
+      new WebpackPwaManifest({
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        theme_color: "#2196f3",
+        icons: [
+          {
+            src: path.resolve("./src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            purpose: "maskable any",
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src/js/sw.js",
+        swDest: "sw.js",
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
